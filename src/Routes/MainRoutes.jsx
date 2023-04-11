636c95d7417258b187e5d40b0af5741e92c9a1d8
NURAY <bekmirovan@gmail.com>
2023-04-11 15:34:28 +0600

    refreshingNavbar2

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -4,6 +4,8 @@ import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import ProductList from "../components/products/ProductList";
 import { Route, Routes } from "react-router-dom";
+import FormOrder from "../components/FormOrder/FormOrder";
+import Auth from "../components/Auth/Auth";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
@@ -11,6 +13,8 @@ const MainRoutes = () => {
     { link: "*", element: <NotFoundPage />, id: 2 },
     { link: "/admin", element: <AdminPage />, id: 3 },
     { link: "/productList", element: <ProductList />, id: 4 },
+    { link: "/formOrder", element: <FormOrder />, id: 5 },
+    { link: "/auth", element: <Auth />, id: 6 },
   ];
   return (
     <div>
