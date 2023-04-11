a61ded855f9c0d009501916283da4508957d1a11
NURAY <bekmirovan@gmail.com>
2023-04-11 13:21:03 +0600

    startingNavbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Navbar/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo-navbar">LOGO</div>
+    </div>
+  );
+};
+
+export default Navbar;
