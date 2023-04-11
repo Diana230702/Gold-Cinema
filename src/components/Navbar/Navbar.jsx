5088f6e5999158d856a1de3a114fd153987beb7d
NURAY <bekmirovan@gmail.com>
2023-04-11 14:18:02 +0600

    refreshingNavbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,12 @@ import "../Navbar/Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "./NavbarImages/LogoNameWhite (1).png";
 
+const LinkStyle = {
+  listStyleType: "none",
+  textDecoration: "none",
+  color: "white",
+};
+
 // ! links----------------------------------------------------------
 const pages = [
   {
@@ -10,8 +16,8 @@ const pages = [
     link: "/",
     id: 1,
   },
-  { name: "AdminPage", link: "/admin", id: 2 },
-  { name: "ProductList", link: "/productList", id: 3 },
+  { name: "Выбрать фильм", link: "/productList", id: 2 },
+  { name: "AdminPage", link: "/admin", id: 3 },
 ];
 // ! links----------------------------------------------------------
 
@@ -19,7 +25,12 @@ const Navbar = () => {
   return (
     <div className="navbar">
       {pages.map((page) => (
-        <Link sx={{ fontSize: "30px" }} key={page.id} to={page.link}>
+        <Link
+          style={LinkStyle}
+          sx={{ fontSize: "30px" }}
+          key={page.id}
+          to={page.link}
+        >
           {page.name}
         </Link>
       ))}
