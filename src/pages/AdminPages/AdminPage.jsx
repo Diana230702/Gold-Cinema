4c233efe371732f0f1c3feea7d5a13058d7390c5
Diana <dianadianadiana586@gmail.com>
2023-04-11 15:20:02 +0600

    f

--- src/pages/AdminPages/AdminPage.jsx
+++ src/pages/AdminPages/AdminPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AdminPage = () => {
-  return <div>AdminPage </div>;
+  return (
+    <div>
+      <h1>Admin Page</h1>
+    </div>
+  );
 };
 
 export default AdminPage;
