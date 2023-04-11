2fb61a2b735c7abb4b03608640fca529be04898d
Diana <dianadianadiana586@gmail.com>
2023-04-11 15:36:14 +0600

    f

--- src/pages/AdminPages/AdminPage.jsx
+++ src/pages/AdminPages/AdminPage.jsx
@@ -4,6 +4,13 @@ const AdminPage = () => {
   return (
     <div>
       <h1>Admin Page</h1>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="description" />
+      <input type="number" placeholder="price" />
+      <input type="text" placeholder="category" />
+      <input type="number" placeholder="year" />
+
+      <input type="file" accept="image/*" placeholder="title" />
     </div>
   );
 };
