22763f23365078cfe5d62ea0e39d8d6d6c7015b8
unknown <samidinovaalbina@gmail.com>
2023-04-17 15:49:55 +0600

    сделано восстановление пароля и добавлены toast

---
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ResetPasswordForm from '../../components/ResetPasswordForm';
+
+const ResetPasswordPage = () => <ResetPasswordForm />
+
+export default ResetPasswordPage;
