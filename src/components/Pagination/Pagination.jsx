3de41767172434373ff0d143161f915dba5a76ea
Nursultan <nursultan14n@gmail.com>
2023-04-18 17:08:27 +0600

    pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,32 +1,51 @@
 import React from "react";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
-
+import "../Pagination/Pagination.css";
 const PaginationList = ({ getPagesCount, currentPage, setCurrentPage }) => {
   //   console.log(getPagesCount);
   return (
     <div>
       {currentPage == 1 ? (
-        <button onClick={() => setCurrentPage(currentPage - 1)} disabled>
+        <button
+          className="btn_pagination"
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled
+        >
           prev
         </button>
       ) : (
-        <button onClick={() => setCurrentPage(currentPage - 1)}>prev</button>
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className="btn_pagination"
+        >
+          prev
+        </button>
       )}
       {getPagesCount().map((item) =>
         item == currentPage ? (
-          <button onClick={() => setCurrentPage(item)} key={item}>
+          <button
+            onClick={() => setCurrentPage(item)}
+            key={item}
+            className="btn_pagination"
+          >
             {item}
           </button>
         ) : (
-          <button onClick={() => setCurrentPage(item)} key={item}>
+          <button
+            onClick={() => setCurrentPage(item)}
+            key={item}
+            className="btn_pagination"
+          >
             {item}
           </button>
         )
       )}
       {getPagesCount().map((item) =>
         item > currentPage ? (
-          <button onClick={() => setCurrentPage(currentPage + 1)} key={item}>
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            key={item}
+            className="btn_pagination"
+          >
             next
             {console.log(item)}
           </button>
